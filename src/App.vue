130cee43fe7573c4b8446f3134f1d05084fc1d98
Roman Matsypula <ukrmatsypula@gmail.com>
2021-12-14 01:10:56 +0200

    added sexond modal with default validation

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
             <Modal
               v-show="modalFirst"
               title="First modal"
-              @close="modalFirst = !modalFirst"
+              @close="modalFirst = false"
             >
               <div slot="body">
                 <p>Text</p>
@@ -22,9 +22,64 @@
             <button
               @click="modalFirst = !modalFirst"
               class="btn btnPrimary"
-              @keydown.esc="modalFirst = !modalFirst"
+              @keydown.esc="modalFirst = false"
             >
               show first modal
+            </button>
+
+            <Modal
+              v-show="modalSecond.show"
+              title="Second modal example"
+              @close="modalSecond.show = false"
+            >
+              <div slot="body">
+                <form @submit.prevent="submitSecondForm">
+                  <label for="name">Name</label>
+                  <input
+                    type="text"
+                    required
+                    id="name"
+                    placeholder="name"
+                    v-model="modalSecond.name"
+                  />
+
+                  <label for="email">Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="email"
+                    v-model="modalSecond.email"
+                  />
+
+                  <button type="submit">Second well done</button>
+                </form>
+              </div>
+              <div slot="footer">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde
+                  rerum facilis tempore dolorem quidem praesentium iusto dolores
+                  impedit sapiente! Eaque ex minus exercitationem ad dolorem
+                  suscipit illum ipsum, esse quo dolorum minima veritatis
+                  temporibus provident, laborum cum itaque unde facere quae
+                  adipisci cupiditate dignissimos aliquid corporis. Dolorem
+                  tempora asperiores inventore rerum quos! Ipsa rem, praesentium
+                  est cum, aut maxime adipisci cupiditate porro laudantium
+                  repudiandae quam aliquid enim beatae, natus molestiae sequi.
+                  Voluptas suscipit, officia, perferendis reprehenderit nostrum
+                  asperiores, voluptate provident cum optio facilis numquam
+                  similique. Animi dicta dolores praesentium officiis, dolorum
+                  non voluptas accusantium doloremque qui magni culpa distinctio
+                  nostrum!
+                </p>
+              </div>
+            </Modal>
+
+            <button
+              @click="modalSecond.show = !modalFirst"
+              class="btn btnPrimary"
+              @keydown.esc="modalSecond.show = false"
+            >
+              show second modal
             </button>
           </div>
         </section>
@@ -43,6 +98,22 @@ export default {
   },
   data: () => ({
     modalFirst: false,
+    modalSecond: {
+      show: false,
+      name: "",
+      email: "",
+    },
   }),
+  methods: {
+    submitSecondForm() {
+      console.log({
+        name: this.modalSecond.name,
+        email: this.modalSecond.email,
+      });
+
+      document.forms[0].reset();
+      this.modalSecond.show = false;
+    },
+  },
 };
 </script>
