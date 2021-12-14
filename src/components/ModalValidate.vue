35854ca9e2696643823ceb7a0bbeae0f86b39111
Roman Matsypula <ukrmatsypula@gmail.com>
2021-12-14 02:32:20 +0200

    added validation for field name

--- src/components/ModalValidate.vue
+++ src/components/ModalValidate.vue
@@ -1,13 +1,26 @@
 <template>
   <Modal title="Second modal example" @close="$emit('closeValidateModal')">
     <div slot="body">
-      <form @submit.prevent="submitSecondForm">
-        <label for="name">Name</label>
-        <input type="text" required id="name" placeholder="name" />
+      <form @submit.prevent="">
+        <div class="form-item" :class="{ errorInput: $v.name.$error }">
+          <label for="name">Name</label>
+          <p class="error-text" v-if="!$v.name.required">Field is required</p>
+          <p class="error-text" v-if="!$v.name.minLength">
+            Name must have at least {{ $v.name.$params.minLength.min }} !
+          </p>
+          <input
+            id="name"
+            placeholder="name"
+            v-model="name"
+            :class="{ error: $v.name.$error }"
+            @change="$v.name.$touch()"
+          />
+        </div>
 
-        <label for="email">Email</label>
-        <input type="email" id="email" placeholder="email" />
-
+        <div class="form-item" :class="{ errorEmail: $v.email.$error }">
+          <label for="email">Email</label>
+          <input id="email" placeholder="email" v-model="email" />
+        </div>
         <button type="submit">Second well done</button>
       </form>
     </div>
@@ -21,13 +34,43 @@
 </template>
 
 <script>
+import { required, minLength, email } from "vuelidate/lib/validators";
+
 import Modal from "@/components/Modal";
 export default {
   name: "ModalValidate",
   components: {
     Modal,
   },
+  data: () => ({
+    email: "",
+    name: "",
+  }),
+  validations: {
+    name: {
+      required,
+      minLength: minLength(4),
+    },
+    email: {
+      required,
+      email,
+    },
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.error-text {
+  margin-bottom: 8px;
+  font-size: 14px;
+  color: red;
+}
+
+.error-text.error-input .error-text {
+  display: block;
+}
+
+.input.error {
+  border-color: red;
+}
+</style>
